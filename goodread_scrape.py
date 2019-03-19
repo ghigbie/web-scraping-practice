a846a72fcf2c7859b4c9edbb65c2cbec8dce7dfd
George Higbie <ghigbie@monigle.com>
2019-03-19 09:33:19 -0600

    Add urls for scraping

--- goodread_scrape.py
+++ goodread_scrape.py
@@ -6,7 +6,18 @@ class GoodReadsSpider(scrapy.Spider)
     def start_requests(self):
         url = ''
         urls = [
-
+            'https://www.goodreads.com/quotes?page=1',
+            'https://www.goodreads.com/quotes?page=2',
+            'https://www.goodreads.com/quotes?page=3',
+            'https://www.goodreads.com/quotes?page=4',
+            'https://www.goodreads.com/quotes?page=5',
+            'https://www.goodreads.com/quotes?page=6',
+            'https://www.goodreads.com/quotes?page=7',
+            'https://www.goodreads.com/quotes?page=8',
+            'https://www.goodreads.com/quotes?page=9',
+            'https://www.goodreads.com/quotes?page=10',
+            'https://www.goodreads.com/quotes?page=11',
+            'https://www.goodreads.com/quotes?page=12'
         ]
 
         for url oin urls:
